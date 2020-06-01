19e4a284066ee1e6304648ca68b4a951eeeac551
Apian <c410113@163.com>
2020-06-01 19:25:34 +0800

    First commit.

---
+++ app/src/main/java/com/kier/flashcards/OneActivity.java
@@ -0,0 +1,16 @@
+package com.kier.flashcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+
+    }
+}
